7d177dd79ebc2d8f9e1d9ee859d77a6ff4fa0ab1
Afif Sahrul M <afifsahrulm91@gmail.com>
2025-11-30 10:09:05 +0700

    menambahkan model pembeli

---
+++ src/drownthsirt/model/Pembeli.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package drownthsirt.model;
+
+/**
+ *
+ * @author afif
+ */
+public class Pembeli {
+    private String tipe;
+    private char ukuran;
+    private String nama;
+    private String alamat;
+    
+    public void setTipe(String tipe){
+        this.tipe = tipe;
+    }
+    
+    public String getTipe(){
+        return tipe;
+    }
+    
+    public void setUkuran(char ukuran){
+        this.ukuran = ukuran;
+    }
+    
+    public char getUkuran(){
+        return ukuran;
+    }
+    
+    public void setNama(String nama){
+        this.nama = nama;
+    }
+    
+    public String getNama(){
+        return nama;
+    }
+    
+    public void setAlamat(String alamat){
+        this.alamat = alamat;
+    }
+    
+    public String getAlamat(){
+        return alamat;
+    }
+    
+    
+    
+    
+    
+    
+    
+}
